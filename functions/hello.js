d5743b8b4ec8e0a2fb9a0d604f79396f9a8c6385
Matt <home@Matts-iMac.local>
2021-10-12 21:12:21 -0400

    working

--- functions/hello.js
+++ functions/hello.js
@@ -18,7 +18,7 @@ exports.handler = async (event) => {
 
   return {
     statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
+    header: { "Access-Control-Allow-Origin": "*" },
     body: JSON.stringify(votesArray),
   };
 };
