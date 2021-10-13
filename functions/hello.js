2a9771e566d81b4f597df54ff65bf208fe4625d9
Matt <home@Matts-iMac.local>
2021-10-12 20:35:53 -0400

    final code 1

--- functions/hello.js
+++ functions/hello.js
@@ -16,8 +16,6 @@ exports.handler = async (event) => {
   }
   console.log(JSON.stringify(votesArray));
 
-  const subject = event.queryStringParameters.name || "World";
-
   return {
     statusCode: 200,
     headers: { "Acess-Control-Allow-Origin": "*" },
