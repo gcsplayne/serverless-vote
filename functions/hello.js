5a6ae83b43a45ce3a86309c07124225310385080
Matt <home@Matts-iMac.local>
2021-10-12 20:34:01 -0400

    final code

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,26 @@
 // functions/hello.js
+let votesArray = [0, 0, 0, 0];
+
 exports.handler = async (event) => {
+  const v = event.queryStringParameters.vote;
+  if (v === "a") {
+    votesArray[0] += 1;
+  } else if (v === "b") {
+    votesArray[1] += 1;
+  } else if (v === "c") {
+    votesArray[2] += 1;
+  } else if (v === "d") {
+    votesArray[1] += 1;
+  } else if (v === "r") {
+    votesArray[(0, 0, 0, 0)];
+  }
+  console.log(JSON.stringify(votesArray));
+
   const subject = event.queryStringParameters.name || "World";
+
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    headers: { "Acess-Control-Allow-Origin": "*" },
+    body: JSON.stringify(votesArray),
   };
 };
